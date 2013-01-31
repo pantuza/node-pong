7c4b56bae4fc2f1863c05a8f5a1475092b19c529
Gustavo Pantuza <gustavopantuza@gmail.com>
2013-01-31 02:24:11 -0200

    Server Modular Namespace
    Created a namespace to represent the server side
    Set private attributes and methods to the namespace
    Created the start method and the server attribute with public access
    Refactored methods and attributes code and name definition

--- src/server.js
+++ src/server.js
@@ -1,95 +1,120 @@
-// Require HTTP module (to start server) and Socket.IO
-var http = require('http'), io = require('/usr/local/lib/node_modules/socket.io');
-
-// Start the server at port 8080
-var server = http.createServer(function(req, res){ 
-
-  // Send HTML headers and message
-  res.writeHead(200,{ 'Content-Type': 'text/html' });
-  res.end('<h1>Hello Socket Lover!</h1>');
-});
-
-// Define socket port to listen 
-server.listen(1234);
-
-// Instantiate socket.io using the created server
-var socket = io.listen(server);
-
-var player1;
-var player2;
-
-var onConnectionCallback = function (client) {
-	
-	// If not exists a player 1, create it
-	if (!player1) {
-		
-		player1 = client;
-		player1.on('message', msgFromPlayer1);
-		player1.on('disconnect', onDisconnect);
-		console.log("player 1 Connected");
-
-	// If not exists player 2, create it
-	} else if (!player2) {
-		
-		player2 = client;
-		player2.on('message', msgFromPlayer2);
-		player2.on('disconnect', onDisconnect);
-		console.log( "player 2 conectado!" );
-
-	// Otherwise, notify client that there are too many user to play
-	} else {
-		client.send({erro:'Too many Users :/'});
-	}
-}
-
-// Connection event
-socket.on('connection', onConnectionCallback); 
-
-/*
- * Player 1 send messages to player 2
- * author : Gustavo Pantuza
- * since	: 09.07.2011
+/**
+ * The Server namespace
  */
-function msgFromPlayer1(msg){
-	
-	if(msg == "start"){
-		sendAll(msg);
-		return
-	}
-	// Send message to player 2
-	player2.send(msg);
-}
-  
-/*
- * Player 2 send messages to player 1
- * author : Gustavo Pantuza
- * since	: 09.07.2011
- */
-function msgFromPlayer2(msg){
+var Server = (function(){
 
-	if (msg == "start"){
-		sendAll(msg);
-		return
-	}
-	// Send message to player 1
-	player1.send(msg);
-}
+    var SOCKET_IO_PATH = '/usr/local/lib/node_modules/socket.io',
+        PORT = 1234,
 
-/*
- * Broadcast message to all clients
- * author: Gustavo Pantuza
- * since: 09.07.2011
- */
-function sendAll(msg){
-    player1.send(msg);
-    player2.send(msg);
-}
+        http = require('http'), 
+        io = require(SOCKET_IO_PATH),
 
-/*
- * When user disconnects, it sends a notification
- * author: Gustavo Pantuza
- * since : 09.07.2011
- */
-function onDisconnect() {
-	console.log('Server has disconnected');
-}
+        server = undefined,
+        socket = undefined,
+        player1 = undefined,
+        player2 = undefined,
+
+        /**
+         * Connection server answer
+         */
+        serverAnswer = function(req, res) {
+
+            // Send HTML headers and message
+            res.writeHead(200,{ 'Content-Type': 'text/html' });
+            res.end('<h1>Hello Socket Lover!</h1>');
+        },
+
+        /*
+         * Player 1 send messages to player 2
+         */
+        msgFromPlayer1 = function (msg){
+            
+            if(msg == "start"){
+                sendAll(msg);
+                return
+            }
+            // Send message to player 2
+            player2.send(msg);
+        },
+          
+        /*
+         * Player 2 send messages to player 1
+         */
+        msgFromPlayer2 = function (msg){
+
+            if (msg == "start"){
+                sendAll(msg);
+                return
+            }
+            // Send message to player 1
+            player1.send(msg);
+        },
+
+        /*
+         * Broadcast message to all clients
+         */
+        sendAll = function (msg){
+            player1.send(msg);
+            player2.send(msg);
+        },
+
+        /**
+         * Client connection callback
+         */
+        onConnectionCallback = function (client) {
+        
+            // If not exists a player 1, create it
+            if (!player1) {
+
+                player1 = client;
+                player1.on('message', msgFromPlayer1);
+                player1.on('disconnect', onDisconnect);
+                console.log("player 1 Connected");
+
+            // If not exists player 2, create it
+            } else if (!player2) {
+                
+                player2 = client;
+                player2.on('message', msgFromPlayer2);
+                player2.on('disconnect', onDisconnect);
+                console.log( "player 2 conectado!" );
+
+            // Otherwise, notify client that there are too many user to play
+            } else {
+                client.send({erro:'Too many Users :/'});
+            }
+        },
+
+        /*
+         * When user disconnects, it sends a notification
+         * author: Gustavo Pantuza
+         * since : 09.07.2011
+         */
+        onDisconnect = function () {
+            console.log('Server has disconnected');
+        },
+
+        /**
+         * Initialization method. Bind the server and socket events
+         */
+        init = function() {
+
+            // Start the server at port 8080
+            server = http.createServer(serverAnswer);
+            server.listen(PORT); // Define socket port to listen 
+
+            // Instantiate socket.io using the created server
+            socket = io.listen(server);
+            socket.on('connection', onConnectionCallback);
+        };
+
+
+        /* NameSpace Public Methods */
+        return {
+            start: function() {
+                return init();
+            },
+
+            server: server
+        }
+})()
