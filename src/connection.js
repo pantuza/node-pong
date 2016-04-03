ab16cab995fa5a0cdfe57b509d6f2a1ec071e413
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2016-04-03 02:40:38 -0300

    Refactored the connection namespace. Public stuff is now using 'this'

--- src/connection.js
+++ src/connection.js
@@ -5,150 +5,133 @@ var Connection = function(game) {
 
     var SERVER_ADDR = '127.0.0.1',
 
-        PORT = 3000,
+    PORT = 3000,
 
-        socket,
+    // player buttons to bind click event
+    playerButtons,
 
-        // Defines which player ('p1', 'p2'). Stores the html element ID
-        playerElm,
+    // loop index
+    i = 0,
 
-        // player buttons to bind click event
-        playerButtons,
+    h3 = undefined,
 
-        // loop index
-        i = 0,
+    that = this;
 
-        h3 = undefined,
+    this.socket = undefined;
 
-        that = this,
-
-
-        /**
-         * Function that send messages to the server
-         * param message: data to send to server
-         */
-        msg = function (message) {
-
-              socket.send(message, socket.id);
-              console.log(message);
-        },
+    // Defines which player ('p1', 'p2'). Stores the html element ID
+    this.playerElm = undefined;
 
 
-        /**
-         * Callback for client connection on server
-         */
-        onConnectCallback = function() {
+    /**
+     * Function that send messages to the server
+     * param message: data to send to server
+     */
+    this.msg = function (message) {
 
-            var text = that.playerElm.id + ' connected';
-            that.playerElm.childNodes[0].textContent = text;
-        },
+          this.socket.send(message, this.socket.id);
+          console.log(message);
+    },
 
 
-        /*
-         * When server sends a message this function is called
-         */
-        onMessageCallback = function(data) {
+    /**
+     * Callback for client connection on server
+     */
+    onConnectCallback = function() {
 
-            /* Triggers the start event to begin the game */
-            if (data === 'start') {
+        var text = that.playerElm.id + ' connected';
+        that.playerElm.childNodes[0].textContent = text;
+    },
 
-                h3.textContent = "The best player win!";
 
-                var startEvent = new Event('start');
-                window.dispatchEvent(startEvent);
+    /*
+     * When server sends a message this function is called
+     */
+    onMessageCallback = function(data) {
 
+        /* Triggers the start event to begin the game */
+        if (data === 'start') {
+
+            h3.textContent = "The best player win!";
+
+            var startEvent = new Event('start');
+            window.dispatchEvent(startEvent);
+
+        } else {
+
+            /* sets the other player positions on canvas */
+            if (that.playerElm.id == 'p1') {
+
+                canvas.player2 = data.p2;
             } else {
 
-                /* sets the other player positions on canvas */
-                if (that.playerElm.id == 'p1') {
-
-                    canvas.player2 = data.p2;
-                } else {
-
-                    canvas.player1 = data.p1;
-                }
+                canvas.player1 = data.p1;
             }
-        },
+        }
+    },
 
 
-        /*
-         * When client disconnect this function is called and shows a 
-         * message to the user that disconnected
-         */
-        onDisconnectCallback = function() {
+    /*
+     * When client disconnect this function is called and shows a 
+     * message to the user that disconnected
+     */
+    onDisconnectCallback = function() {
 
-            var text = that.playerElm.id + ' disconnected';
-            that.playerElm.childNodes[0].textContent = text;
-        },
+        var text = that.playerElm.id + ' disconnected';
+        that.playerElm.childNodes[0].textContent = text;
+    },
 
 
-        /**
-         * Function that connects to the server
-         * param element: html element id to be connected ('p1' or 'p2')
-         */
-        connect = function(event) {
+    /**
+     * Function that connects to the server
+     * param element: html element id to be connected ('p1' or 'p2')
+     */
+    this.connect = function(event) {
 
-            var listener = function () {
-                return false;
-            };
-
-            that.playerElm = this.parentElement;
-
-            /* unbind the other button to not let the user create
-             * another player
-             */
-            if(that.playerElm.id === 'p1') {
-
-                playerButtons[1].removeEventListener('click', listener);
-            } else {
-
-                playerButtons[0].removeEventListener('click', listener);
-            }
-
-            /* Creates an IO Socket with the server address binding on PORT */
-            socket = new io(SERVER_ADDR + ":" + PORT);
-
-            socket.on('connect', onConnectCallback);
-            socket.on('message', onMessageCallback);
-            socket.on('disconnect', onDisconnectCallback);
-
-            socket.connect();
-
-            event.preventDefault();
-            event.stopPropagation();
+        var listener = function () {
+            return false;
         };
 
+        that.playerElm = this.parentElement;
 
-        playerButtons = document.querySelectorAll("#p1 a, #p2 a");
+        /* unbind the other button to not let the user create
+         * another player
+         */
+        if(that.playerElm.id === 'p1') {
 
-        for( ; i < playerButtons.length; i++) {
+            playerButtons[1].removeEventListener('click', listener);
+        } else {
 
-            playerButtons[i].addEventListener('click', connect, false);
+            playerButtons[0].removeEventListener('click', listener);
         }
 
-        startButton = document.getElementById('start');
-        startButton.addEventListener('click', function (event) {
-            msg('start');
-            event.preventDefault();
-            event.stopPropagation();
-        });
+        /* Creates an IO Socket with the server address binding on PORT */
+        that.socket = new io(SERVER_ADDR + ":" + PORT);
 
-        h3 = document.getElementById('canvas_header');
+        that.socket.on('connect', onConnectCallback);
+        that.socket.on('message', onMessageCallback);
+        that.socket.on('disconnect', onDisconnectCallback);
+
+        that.socket.connect();
+
+        event.preventDefault();
+        event.stopPropagation();
+    };
 
 
-    /* Name Space Public methods and attributes */
-    return {
-        connect: function(elm) {
-            return connect(elm);
-        },
+    playerButtons = document.querySelectorAll("#p1 a, #p2 a");
 
-        socket: socket,
+    for( ; i < playerButtons.length; i++) {
 
-        msg: function (message) {
-            return msg(message);
-        },
-
-        playerElm: playerElm
-
+        playerButtons[i].addEventListener('click', this.connect, false);
     }
+
+    startButton = document.getElementById('start');
+    startButton.addEventListener('click', function (event) {
+        that.msg('start');
+        event.preventDefault();
+        event.stopPropagation();
+    });
+
+    h3 = document.getElementById('canvas_header');
 };
